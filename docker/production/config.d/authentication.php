5575a52c9ed2ab2081216ff39717d91ca00e7596
Guilherme Silva <guilhermemendessilva123@gmail.com>
2025-05-21 21:25:00 +0000

    login ambiente de producao

--- docker/production/config.d/authentication.php
+++ docker/production/config.d/authentication.php
@@ -11,6 +11,42 @@ return [
     'auth.config' => array(
         'salt' => env('AUTH_SALT', null),
         'timeout' => '24 hours',
+        //Habilita registro e login através do CPF
+        'enableLoginByCPF' => true,
+            
+        //apelido do metadata que será salvo o campo CPF
+        'metadataFieldCPF' => 'documento',
+        
+        //Regra para saber se o usuario deve ou não confiar o email para poder utilizar o sistema
+        'userMustConfirmEmailToUseTheSystem' => false,
+
+        //Regra de força de senha - Ter no mínimo 1 letra maiúscula
+        'passwordMustHaveCapitalLetters' => true,
+
+        //Regra de força de senha - Ter no mínimo 1 letra minúscula
+        'passwordMustHaveLowercaseLetters' => true,
+
+        //Regra de força de senha - Ter no mínimo 1 caractere especial
+        'passwordMustHaveSpecialCharacters' => true,
+
+        //Regra de força de senha - Ter no mínimo 1 caractere numérico
+        'passwordMustHaveNumbers' => true,
+
+        //Regra de força de senha - Ter no mínimo n caracteres
+        'minimumPasswordLength' => 6,
+        
+        //Configuração de GOOGLE Recaptcha
+        'google-recaptcha-secret' => '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe',
+        'google-recaptcha-sitekey' => '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
+
+        //Tempo da sessao do usuario em segundos
+        'sessionTime' => 7200,
+
+        //Limite de tentativas não sucedidas de login antes de bloquear o usuario por X minutos
+        'numberloginAttemp' => '5', 
+
+        //Tempo de bloqueio do usuario em segundos, após romper limites de tentativas não sucedidas
+        'timeBlockedloginAttemp' => '900',
         'strategies' => [
             'Facebook' => array(
                'app_id' => env('AUTH_FACEBOOK_APP_ID', null),
